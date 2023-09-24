4e030ef076950b99828dd72519055b28129c4212
mitosagi <54105954+mitosagi@users.noreply.github.com>
2023-09-24 11:23:57 +0900

    update prompt

--- pri_infer.py
+++ pri_infer.py
@@ -14,7 +14,7 @@ def pri_infer(info_prompt, isAPI=True):
         read_text('workspace/metadata.yaml').strip(),
         separater,
         read_text('pri_infer.txt').strip(),
-        'ここまでの情報からjson文字列を生成、validate_by_json_schemaにより検証してください。' if isAPI else 'これらの情報からjsonファイルを生成し上記のjson schemaによりエラーを出力して検証を行ってください。エラーのある場合はリトライしてください。その後、完成したjsonファイルのダウンロードリンクを提供してください。もし、json.dump()を使用する場合は ensure_ascii=Falseを指定してください。'])
+        'ここまでの情報からjson文字列を生成、validate_by_json_schemaにより検証してください。' if isAPI else 'これらの情報からjsonファイルを生成し上記のjson schemaによりエラーを出力して検証を行ってください。エラーのある場合はリトライしてください。その後、完成したjsonファイルのダウンロードリンクを提供してください。Pythonにschemaを入力する際はFalseの先頭を大文字にしてください。もし、json.dump()を使用する場合は ensure_ascii=Falseを指定してください。'])
 
 
 if __name__ == '__main__':
